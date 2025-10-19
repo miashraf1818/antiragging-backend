12adebd6cd189971ab303f441b635f36c4e3844a
miashraf1818 <miashrafmiashraf18@gmail.com>
2025-10-20 00:44:38 +0530

    Update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@ cat > build.sh << 'EOF'
 #!/usr/bin/env bash
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
+# Install requirements
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
 EOF
