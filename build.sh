d182975975d9dbbcc24d4769eeaf7e647f4e11b6
miashraf1818 <miashrafmiashraf18@gmail.com>
2025-10-20 01:13:22 +0530

    Add poetry install to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Installing dependencies with Poetry..."
+poetry install --no-dev
+
+echo "Collecting static files..."
 python manage.py collectstatic --no-input --clear
+
+echo "Running migrations..."
 python manage.py migrate
